c6308f4c013db1610ada2c27e02339b76e457a88
yasminab23-meet <109899977+yasminab23-meet@users.noreply.github.com>
2022-07-31 14:32:15 +0300

    Starting Firebase Authentication Lab

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,9 +2,33 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+firebaseConfig = {
+  "apiKey": "AIzaSyBQ3uszrjXJTmMbRtR910reV1pHbZVAghk",
+  "authDomain": "fir-authentication-lab-37df7.firebaseapp.com",
+  "projectId": "fir-authentication-lab-37df7",
+  "storageBucket": "fir-authentication-lab-37df7.appspot.com",
+  "messagingSenderId": "136373746316",
+  "appId": "1:136373746316:web:62f123fcc74a01f860d9a9"
+}
+
+firebase =pyrebase.initalize_app(firebaseConfig)
+auth = firebase.auth()
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
+firebaseConfig = {
+  "apiKey": "AIzaSyBQ3uszrjXJTmMbRtR910reV1pHbZVAghk",
+  "authDomain": "fir-authentication-lab-37df7.firebaseapp.com",
+  "projectId": "fir-authentication-lab-37df7",
+  "storageBucket": "fir-authentication-lab-37df7.appspot.com",
+  "messagingSenderId": "136373746316",
+  "appId": "1:136373746316:web:62f123fcc74a01f860d9a9"
+}
+
+firebase =pyrebase.initalize_app(firebaseConfig)
+auth = firebase.auth()
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
